0ab670c5c3a1b1f4c553ac34fa3d2aa81b7ef29a
ffluy23 <ffluy23@gmail.com>
2026-05-02 15:18:32 +0900

    시간

--- api/start-battle.js
+++ api/start-battle.js
@@ -65,7 +65,7 @@ export default async function handler(req, res) {
     await batch.commit()
 
     // intro_done: true 세팅
-    await roomRef.update({ intro_done: true })
+    await roomRef.update({ intro_done: true, game_started_at: Date.now() })
 
     return res.status(200).json({ ok: true })
 
